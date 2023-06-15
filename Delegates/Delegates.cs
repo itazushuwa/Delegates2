f1cd06b2d7996cce37fc0e0f2a84e44ee4246233
itazushuwa <demonize4532@gmail.com>
2023-06-15 08:23:57 +0700

    Добавьте файлы проекта.

---
+++ Delegates/Delegates.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Delegates
+    {
+        delegate double Operations(double x);
+
+        private Operations[] array = { Add, Substract, Multiply, Divide };
+
+        public void GoThroughtArray(double x)
+        {
+            foreach (Operations op in array)
+            {
+                Console.WriteLine(op(x));
+            }
+        }
+        static double Divide(double x) => x / x;
+        static double Multiply(double x) => Math.Round(x * x, 5);
+        static double Add(double x) => Math.Round(x + x, 5);
+        static double Substract(double x) => x - x;
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Delegates;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Delegates del = new Delegates();
+            Console.WriteLine("Введите число типа Double");
+            del.GoThroughtArray(double.Parse(Console.ReadLine()));
+        }
+    }
+}
